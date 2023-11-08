5ad7c54fee8d19a5dbf76598f342d4f312f6145b
dmden22 <maslovden22@yandex.ru>
2023-11-08 16:26:22 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +16,81 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS userTable(" +
+                    "id bigint auto_increment primary key, " +
+                    "name varchar(255) null, " +
+                    "lastname varchar(255) null, " +
+                    "age tinyint null);");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
     }
 
     public void dropUsersTable() {
-
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("drop table if exists usertable;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("insert usertable(name, lastname, age) " +
+                    "values('" + name + "','" + lastName + "'," + age + ");");
+            System.out.println("User с именем " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
     }
 
     public void removeUserById(long id) {
-
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("delete from usertable where id=" + id + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> resultList = new ArrayList<>();
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from usertable;");
+            while (resultSet.next()) {
+                User tmpUser = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
+                tmpUser.setId(resultSet.getLong(1));
+                resultList.add(tmpUser);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
+        return resultList;
     }
 
     public void cleanUsersTable() {
-
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("TRUNCATE TABLE usertable;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Util.closeConnection(connection);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL_DB = "jdbc:mysql://localhost:3306/predb";
+    private static final String USER = "test1";
+    private static final String PSW = "test1";
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL_DB, USER, PSW);
+            if (!connection.isClosed()) {
+                return connection;
+            }
+        } catch (SQLException e) {
+            System.err.println("Проблемы с драйвером подключения к БД");
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection == null || connection.isClosed()) {
+                return;
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("При закрытии соединения возникла ошибка!");
+        }
+    }
 }
