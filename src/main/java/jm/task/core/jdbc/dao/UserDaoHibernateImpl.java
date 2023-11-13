6129b12cee9562417a4f5c74f02c40d29ed38a0e
dmden22 <maslovden22@yandex.ru>
2023-11-13 13:45:45 +0300

    Hibernate done v1.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -10,34 +17,83 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        String createUsersQuery = "CREATE TABLE IF NOT EXISTS userTable(" +
+                "id bigint auto_increment primary key, " +
+                "name varchar(255) null, " +
+                "lastname varchar(255) null, " +
+                "age tinyint null)";
 
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(createUsersQuery).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String dropUsersQuery = "DROP TABLE IF EXISTS usertable;";
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(dropUsersQuery).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User с именем " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            User userToDelete = session.find(User.class, id);
+            session.remove(userToDelete);
+            session.flush();
+            session.clear();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> resultListUsers = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            resultListUsers = session.createQuery("FROM " + User.class.getSimpleName()).list();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultListUsers;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String cleanTableQuery = "TRUNCATE TABLE usertable;";
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(cleanTableQuery).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -11,85 +11,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private final String createUsersQuery = "CREATE TABLE IF NOT EXISTS userTable(" +//todo: codeStyle
-            "id bigint auto_increment primary key, " +
-            "name varchar(255) null, " +
-            "lastname varchar(255) null, " +
-            "age tinyint null)";
-
     public void createUsersTable() {
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
+        String createUsersQuery = "CREATE TABLE IF NOT EXISTS userTable(" +
+                "id bigint auto_increment primary key, " +
+                "name varchar(255) null, " +
+                "lastname varchar(255) null, " +
+                "age tinyint null)";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
             statement.execute(createUsersQuery);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
     }
 
     public void dropUsersTable() {
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("drop table if exists usertable;");
+        String dropUsersQuery = "drop table if exists usertable;";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            statement.execute(dropUsersQuery);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("insert usertable(name, lastname, age) " +
-                    "values('" + name + "','" + lastName + "'," + age + ");");
+        String saveUserInTableQuery = "insert usertable(name, lastname, age) " +
+                "values('" + name + "','" + lastName + "'," + age + ");";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            statement.execute(saveUserInTableQuery);
             System.out.println("User с именем " + name + " добавлен в базу данных");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
     }
 
     public void removeUserById(long id) {
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("delete from usertable where id=" + id + ";");
+        String removeUserQuery = "delete from usertable where id=" + id + ";";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            statement.execute(removeUserQuery);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
     }
 
     public List<User> getAllUsers() {
-        List<User> resultList = new ArrayList<>();
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from usertable;");
+        List<User> usersList = new ArrayList<>();
+        String resultSetQuery = "select * from usertable;";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            ResultSet resultSet = statement.executeQuery(resultSetQuery);
             while (resultSet.next()) {
                 User tmpUser = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
                 tmpUser.setId(resultSet.getLong(1));
-                resultList.add(tmpUser);
+                usersList.add(tmpUser);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
-        return resultList;
+        return usersList;
     }
 
     public void cleanUsersTable() {
-        Connection connection = Util.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("TRUNCATE TABLE usertable;");
+        String cleanTableQuery = "TRUNCATE TABLE usertable;";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            statement.execute(cleanTableQuery);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        Util.closeConnection(connection);
     }
 }
