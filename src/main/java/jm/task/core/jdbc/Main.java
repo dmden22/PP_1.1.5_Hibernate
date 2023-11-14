84c9d8ce4989a45ab1bcf96466d98d3ec71477f8
dmden22 <maslovden22@yandex.ru>
2023-11-14 14:11:37 +0300

    delete JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,28 +2,9 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
-
-        userDao.createUsersTable();
-
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
-
-        userDao.removeUserById(1);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();*/
 
         UserDao userDao = new UserDaoHibernateImpl();
         userDao.createUsersTable();
@@ -35,10 +16,8 @@ public class Main {
 
         userDao.removeUserById(2);
         userDao.cleanUsersTable();
-        userDao.getAllUsers().stream().forEach(System.out::println);
+        userDao.getAllUsers().forEach(System.out::println);
         userDao.dropUsersTable();
-
-
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,44 +7,9 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static final String URL_DB = "jdbc:mysql://localhost:3306/predb";
-    private static final String USER = "test1";
-    private static final String PSW = "test1";
-
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-
-            connection = DriverManager.getConnection(URL_DB, USER, PSW);
-            if (!connection.isClosed()) {
-                return connection;
-            }
-        } catch (SQLException e) {
-            System.err.println("Проблемы с драйвером подключения к БД");
-        }
-        return connection;
-    }
-
-    public static void closeConnection(Connection connection) {
-        try {
-            if (connection == null || connection.isClosed()) {
-                return;
-            }
-            connection.close();
-        } catch (SQLException e) {
-            System.err.println("При закрытии соединения возникла ошибка!");
-        }
-    }
-
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
@@ -52,7 +17,6 @@ public class Util {
             try {
                 Configuration configuration = new Configuration();
 
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/predb?useSSL=false");
@@ -62,9 +26,6 @@ public class Util {
 
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.FORMAT_SQL, "true");
-
-                //settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                //settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
                 configuration.setProperties(settings);
                 configuration.addAnnotatedClass(User.class);
